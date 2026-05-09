290bc69373ef372f75636d0890ce9fc149d8faf3
Keerthana <kvr9143@gmail.com>
2026-05-09 06:08:41 +0530

    Updated ChatWindow

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -26,9 +26,8 @@ function ChatWindow(){
         };
 
         try{
-            // const response = await fetch("http://localhost:8080/api/chat", options);
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/chat`, options);
-            const res = await response.json()
+            const response = await fetch("http://localhost:8080/api/chat", options);
+            const res = await response.json();
             console.log(res);
             setReply(res.reply);
         }catch(err){
