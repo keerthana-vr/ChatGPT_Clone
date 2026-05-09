9ddd76ed759eb7f9a6b42976c18f9d24006749ae
Keerthana <kvr9143@gmail.com>
2026-05-09 06:54:01 +0530

    Final Changes

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -27,7 +27,7 @@ function ChatWindow(){
 
         try{
             // const response = await fetch("http://localhost:8080/api/chat", options);
-            const response = await fetch("https://chatgpt-clone-backend-un4u.onrender.com/", options);
+            const response = await fetch("https://chatgpt-clone-backend-un4u.onrender.com/api/chat", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
