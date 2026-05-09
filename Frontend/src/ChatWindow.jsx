8492383c597bb9ced01a774e33b3391c06f62780
Keerthana <kvr9143@gmail.com>
2026-05-09 06:24:09 +0530

    Updated ChatWindow url

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -26,7 +26,8 @@ function ChatWindow(){
         };
 
         try{
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            // const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch("https://onrender.com", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
